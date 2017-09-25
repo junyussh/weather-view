78e280223ef446542ff4e28e5990542a50e4bd77
Chun Yu <junyussh@gmail.com>
2017-09-26 03:31:03 +0800

    RWD Bug 修到快崩潰 去你的 CSS media query 寫了 RWD 版本的 Sidebar 快累死我了，幹到身心俱疲，快死掉了，希望明天能起得來

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,15 +1,22 @@
 var apiurl = [{
     name: "NHSH",
-    url: "http://203.72.63.54:8080/api"
+    url: "http://203.72.63.54:5000/api"
 }];
 
 var cards = new Vue({
     el: '.station-list',
     data: {
         message: "Hello",
-        items: []
+        items: [
+            {"location": "內湖高中", "temperature": "20", "humidity": "20", "light": "2", "UV": "5", "rainfall": "200", "time": "2017-09-18T04:45:15.650Z" }, 
+            {"location": "北一女中", "temperature": "21", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
+            {"location": "中山女高", "temperature": "22", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
+            {"location": "景美女中", "temperature": "23", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
+            {"location": "華僑中學", "temperature": "24", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
+        ]
     },
     beforeMount: function() {
+        /*
         fetch(apiurl[0].url, {
             method: "GET"
         }).then((res) => {
@@ -21,6 +28,7 @@ var cards = new Vue({
             console.log("err");
             console.log(err);
         })
+        */
     }
 })
 Vue.component("cards", {
