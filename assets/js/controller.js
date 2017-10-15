1a85bd059e14c727fd225848f007b1cc0f324bc7
Chun Yu <junyussh@gmail.com>
2017-10-16 01:41:26 +0800

    Add parts of code

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -2,17 +2,20 @@ var apiurl = [{
     name: "NHSH",
     url: "http://203.72.63.54:5000/api"
 }];
-
+Vue.component('modal', {
+  template: require('./modal.vue')
+})
 var cards = new Vue({
     el: '.station-list',
     data: {
         message: "Hello",
+        showModal: false,
         items: [
-            { "location": "內湖高中", "temperature": "32", "humidity": "20", "light": "2", "UV": "5", "rainfall": "200", "time": "2017-09-18T04:45:15.650Z" },
             //{"location": "北一女中", "temperature": "21", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
             //{"location": "中山女高", "temperature": "22", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
             //{"location": "景美女中", "temperature": "23", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" }, 
             { "location": "華僑中學", "temperature": "34", "humidity": "2", "light": "4", "UV": "3", "rainfall": "5" },
+            { "location": "內湖高中", "temperature": "32", "humidity": "20", "light": "2", "UV": "5", "rainfall": "200", "time": "2017-09-18T04:45:15.650Z" }
         ]
     },
     beforeMount: function() {
@@ -31,20 +34,15 @@ var cards = new Vue({
         */
     },
     methods: {
-        showModal: function(location) {
-            console.log(location);
+        showMessage: function(location) {
             var data={};
             this.items.map(function(e) {
                 if (e.location == location) {
                     return data = e;
                 }
             });
+            this.showModal == true;
             alert(data.temperature);
         }
-    }
-})
-Vue.component("cards", {
-    render(h) {
-
     }
 })
